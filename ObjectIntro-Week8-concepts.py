ff52e8ea845ddc1d06e2dd0164908d8a8fbd5b94
Daniel Wroblewski <perfmaster1@gmail.com>
2025-08-10 22:48:03 -0500

    All code appears working - last commmit for today 8/10/2025

--- ObjectIntro-Week8-concepts.py
+++ ObjectIntro-Week8-concepts.py
@@ -33,6 +33,10 @@ class Node(object):
         self.next_node = new_next
 
 #==========================================================================================
+#    -- End of Node class definition and methods --
+#========================================================================================== 
+
+#==========================================================================================
 #   ll_Nodes_Create: Create the linked list structure with inputs of head and list. Return
 #     the head node. 
 #
@@ -100,6 +104,62 @@ def find_node(head=None,remove=None):
     prev_node.next_node=cur_node.get_next()   # cuts out cur_node from the linked list
     return head
 
+#=================================================================================================================
+#   find_node_insert: Find the node to insert AFTER, and insert a new node in the node structure 
+#=================================================================================================================
+def find_node_insert(mode="x",head=None,insert=None,prevVal=""):
+    mode=mode.lower()
+    if mode not in ["h","1","l","e"]:
+        print("\n<<<<< CONDITION: Invalid mode \"",mode,"\" requested, mode not implemented, no action taken >>>>>",sep='')
+        return head
+    
+    if insert is None:
+        print("!!!!! ERROR: Empty insert value found or not specified, insert not performed - skipping !!!!!\n")
+        return head  
+
+    if head == None:
+        print("!!!!! ERROR: Invalid head found or not specified, insert not performed - skipping !!!!!\n")
+        return head 
+    
+    rootNode=head                              # anchor point for the operation
+    newNode=Node(insert)                       # all modes need a new node added     
+    if mode == "h":                            # a new head is requested, replacing the old head
+        print("Inserting/replacing the head node\n")
+        newNode.next_node=rootNode
+        print("\nA new head was created for this linked list. The old head is: ",rootNode.get_data())
+        print("   The new head and next relationships are ",newNode.get_data(),newNode.next_node.get_data())
+        return newNode
+
+    if mode == "1":                             # add list[1] mode is requested, keep the rest of the nodes
+        print("\nA new Node 1 item will be created for this linked list. The old head and Node 1 is: ",rootNode.data,rootNode.next_node.get_data())
+        newNode2=rootNode.get_next()            # next_node
+        newNode.next_node=rootNode.get_next()   # next_node
+        rootNode.next_node=newNode
+        node2=newNode.get_next()
+        print("   The new Head, Node 1 & 2 is ",rootNode.get_data(), newNode.get_data(),node2.get_data())
+        return rootNode
+    
+    if mode == "l":                             # a new LAST NODE is requested
+        print("\nA new End Node item called ",insert," will be created for this linked list.")
+        newNode2=rootNode.get_next()
+        while newNode2.next_node is not None:
+            newNode2=newNode2.get_next()        # cycle through nodes until last node in Linked List
+        newNode2.set_next(newNode)
+        return rootNode        
+
+    if mode == "e":                            # add list item at specified position is requested, keep the rest of the nodes
+        print("\nA new Node item called ",insert," will be inserted after the ",prevVal," value for this linked list.")
+        newNode2=rootNode.get_next()           # next_node
+        while newNode2.get_data() != prevVal:
+            newNode2=newNode2.get_next()        # cycle through nodes until the node-before-insert value is found
+        nextNodeBkup = newNode2.next_node
+        newNode2.next_node = newNode
+        newNode.next_node = nextNodeBkup
+        return rootNode 
+       
+    return
+
+
 #==========================================================================================
 #   printnodes: print the nodes starting with the head, sequentially: 
 #==========================================================================================
@@ -130,7 +190,7 @@ def printnodes(index,mnode,eflag,tstmsg):
     exit
 
 #==========================================================================================
-#    -- end of methods / functions --
+#    -- End of functions for Node Class --
 #========================================================================================== 
 
 def main():
@@ -174,10 +234,22 @@ def main():
 #--------------------------------------------------------------
 
     rm_index=2
-
+    print("  Attempting to remove node ",rm_index+1," \n")
     if not maxIndexCheck(maxindex,rm_index):
         remnode=Node(nodelist[rm_index])
         mynode=find_node(mynode,remnode)
+
+
+
+    mynode = find_node_insert("h",mynode,"Xth Node","")
+
+    mynode = find_node_insert("1",mynode,"Ath Node","")
+
+    mynode = find_node_insert("l",mynode,"TheEndOfAllNodes","")   
+
+    mynode = find_node_insert("e",mynode,"TheSecondThirdNode","Second node")   
+
+    mynode = find_node_insert("X",mynode,"ThePhantomNode","")   
 
     testmsg="\n |==============================================================================|>>>\n   Node print list result after all operations completed: "
     printnodes(idx,mynode,endflag,testmsg)
